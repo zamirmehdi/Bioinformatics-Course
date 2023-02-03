d6e2a747228412facea6e0acd3d3c3c382ff0b25
zamirmehdi <amzarrinnezhad@gmail.com>
2023-02-03 22:57:03 +0330

    Added HW2 Programming files.

---
+++ 2- Pairwise Sequence Alignment/Programming/Code/semi_global_alignment.py
@@ -0,0 +1,199 @@
+PAM250 = {
+    'A': {'A': 2, 'C': -2, 'D': 0, 'E': 0, 'F': -3, 'G': 1, 'H': -1, 'I': -1, 'K': -1, 'L': -2, 'M': -1, 'N': 0, 'P': 1,
+          'Q': 0, 'R': -2, 'S': 1, 'T': 1, 'V': 0, 'W': -6, 'Y': -3},
+    'C': {'A': -2, 'C': 12, 'D': -5, 'E': -5, 'F': -4, 'G': -3, 'H': -3, 'I': -2, 'K': -5, 'L': -6, 'M': -5, 'N': -4,
+          'P': -3, 'Q': -5, 'R': -4, 'S': 0, 'T': -2, 'V': -2, 'W': -8, 'Y': 0},
+    'D': {'A': 0, 'C': -5, 'D': 4, 'E': 3, 'F': -6, 'G': 1, 'H': 1, 'I': -2, 'K': 0, 'L': -4, 'M': -3, 'N': 2, 'P': -1,
+          'Q': 2, 'R': -1, 'S': 0, 'T': 0, 'V': -2, 'W': -7, 'Y': -4},
+    'E': {'A': 0, 'C': -5, 'D': 3, 'E': 4, 'F': -5, 'G': 0, 'H': 1, 'I': -2, 'K': 0, 'L': -3, 'M': -2, 'N': 1, 'P': -1,
+          'Q': 2, 'R': -1, 'S': 0, 'T': 0, 'V': -2, 'W': -7, 'Y': -4},
+    'F': {'A': -3, 'C': -4, 'D': -6, 'E': -5, 'F': 9, 'G': -5, 'H': -2, 'I': 1, 'K': -5, 'L': 2, 'M': 0, 'N': -3,
+          'P': -5, 'Q': -5, 'R': -4, 'S': -3, 'T': -3, 'V': -1, 'W': 0, 'Y': 7},
+    'G': {'A': 1, 'C': -3, 'D': 1, 'E': 0, 'F': -5, 'G': 5, 'H': -2, 'I': -3, 'K': -2, 'L': -4, 'M': -3, 'N': 0, 'P': 0,
+          'Q': -1, 'R': -3, 'S': 1, 'T': 0, 'V': -1, 'W': -7, 'Y': -5},
+    'H': {'A': -1, 'C': -3, 'D': 1, 'E': 1, 'F': -2, 'G': -2, 'H': 6, 'I': -2, 'K': 0, 'L': -2, 'M': -2, 'N': 2, 'P': 0,
+          'Q': 3, 'R': 2, 'S': -1, 'T': -1, 'V': -2, 'W': -3, 'Y': 0},
+    'I': {'A': -1, 'C': -2, 'D': -2, 'E': -2, 'F': 1, 'G': -3, 'H': -2, 'I': 5, 'K': -2, 'L': 2, 'M': 2, 'N': -2,
+          'P': -2, 'Q': -2, 'R': -2, 'S': -1, 'T': 0, 'V': 4, 'W': -5, 'Y': -1},
+    'K': {'A': -1, 'C': -5, 'D': 0, 'E': 0, 'F': -5, 'G': -2, 'H': 0, 'I': -2, 'K': 5, 'L': -3, 'M': 0, 'N': 1, 'P': -1,
+          'Q': 1, 'R': 3, 'S': 0, 'T': 0, 'V': -2, 'W': -3, 'Y': -4},
+    'L': {'A': -2, 'C': -6, 'D': -4, 'E': -3, 'F': 2, 'G': -4, 'H': -2, 'I': 2, 'K': -3, 'L': 6, 'M': 4, 'N': -3,
+          'P': -3, 'Q': -2, 'R': -3, 'S': -3, 'T': -2, 'V': 2, 'W': -2, 'Y': -1},
+    'M': {'A': -1, 'C': -5, 'D': -3, 'E': -2, 'F': 0, 'G': -3, 'H': -2, 'I': 2, 'K': 0, 'L': 4, 'M': 6, 'N': -2,
+          'P': -2, 'Q': -1, 'R': 0, 'S': -2, 'T': -1, 'V': 2, 'W': -4, 'Y': -2},
+    'N': {'A': 0, 'C': -4, 'D': 2, 'E': 1, 'F': -3, 'G': 0, 'H': 2, 'I': -2, 'K': 1, 'L': -3, 'M': -2, 'N': 2, 'P': 0,
+          'Q': 1, 'R': 0, 'S': 1, 'T': 0, 'V': -2, 'W': -4, 'Y': -2},
+    'P': {'A': 1, 'C': -3, 'D': -1, 'E': -1, 'F': -5, 'G': 0, 'H': 0, 'I': -2, 'K': -1, 'L': -3, 'M': -2, 'N': 0,
+          'P': 6, 'Q': 0, 'R': 0, 'S': 1, 'T': 0, 'V': -1, 'W': -6, 'Y': -5},
+    'Q': {'A': 0, 'C': -5, 'D': 2, 'E': 2, 'F': -5, 'G': -1, 'H': 3, 'I': -2, 'K': 1, 'L': -2, 'M': -1, 'N': 1, 'P': 0,
+          'Q': 4, 'R': 1, 'S': -1, 'T': -1, 'V': -2, 'W': -5, 'Y': -4},
+    'R': {'A': -2, 'C': -4, 'D': -1, 'E': -1, 'F': -4, 'G': -3, 'H': 2, 'I': -2, 'K': 3, 'L': -3, 'M': 0, 'N': 0,
+          'P': 0, 'Q': 1, 'R': 6, 'S': 0, 'T': -1, 'V': -2, 'W': 2, 'Y': -4},
+    'S': {'A': 1, 'C': 0, 'D': 0, 'E': 0, 'F': -3, 'G': 1, 'H': -1, 'I': -1, 'K': 0, 'L': -3, 'M': -2, 'N': 1, 'P': 1,
+          'Q': -1, 'R': 0, 'S': 2, 'T': 1, 'V': -1, 'W': -2, 'Y': -3},
+    'T': {'A': 1, 'C': -2, 'D': 0, 'E': 0, 'F': -3, 'G': 0, 'H': -1, 'I': 0, 'K': 0, 'L': -2, 'M': -1, 'N': 0, 'P': 0,
+          'Q': -1, 'R': -1, 'S': 1, 'T': 3, 'V': 0, 'W': -5, 'Y': -3},
+    'V': {'A': 0, 'C': -2, 'D': -2, 'E': -2, 'F': -1, 'G': -1, 'H': -2, 'I': 4, 'K': -2, 'L': 2, 'M': 2, 'N': -2,
+          'P': -1, 'Q': -2, 'R': -2, 'S': -1, 'T': 0, 'V': 4, 'W': -6, 'Y': -2},
+    'W': {'A': -6, 'C': -8, 'D': -7, 'E': -7, 'F': 0, 'G': -7, 'H': -3, 'I': -5, 'K': -3, 'L': -2, 'M': -4, 'N': -4,
+          'P': -6, 'Q': -5, 'R': 2, 'S': -2, 'T': -5, 'V': -6, 'W': 17, 'Y': 0},
+    'Y': {'A': -3, 'C': 0, 'D': -4, 'E': -4, 'F': 7, 'G': -5, 'H': 0, 'I': -1, 'K': -4, 'L': -1, 'M': -2, 'N': -2,
+          'P': -5, 'Q': -4, 'R': -4, 'S': -3, 'T': -3, 'V': -2, 'W': 0, 'Y': 10}
+}
+GAP_PENALTY = -9
+score_matrix = []
+direction_matrix = []
+total_score = 0
+total_locations = []
+seq = []
+
+
+def print_output(score, seq):
+    print(score)
+    sorted_seq = [i[0] + i[1] for i in seq]
+    sorted_seq.sort()
+    for i in sorted_seq:
+        print(i[0:int(len(i) / 2)])
+        print(i[int(len(i) / 2):])
+
+
+def trace_back(strings, x, y):
+    directions = direction_matrix[x][y]
+
+    for direction in directions:
+        next_cell = ()
+        if direction == 'd':
+            next_cell = (x - 1, y - 1)
+        if direction == 'h':
+            next_cell = (x, y - 1)
+        if direction == 'v':
+            next_cell = (x - 1, y)
+
+        if next_cell[0] == 0 or next_cell[1] == 0:
+            string1 = strings[0]
+            string2 = strings[1]
+
+            # TODO add gaps to start of strings if needed
+            for i in range(x - 2, -1, -1):
+                string1 = string1 + str1[i]
+                string2 = string2 + '-'
+            for i in range(y - 2, -1, -1):
+                string1 = string1 + '-'
+                string2 = string2 + str2[i]
+
+            string1 = string1[::-1]
+            string2 = string2[::-1]
+
+            seq.append((string1, string2))
+        else:
+            if direction == 'd':
+                new_string1 = strings[0] + str1[next_cell[0] - 1]
+                new_string2 = strings[1] + str2[next_cell[1] - 1]
+                trace_back((new_string1, new_string2), x - 1, y - 1)
+            elif direction == 'h':
+                new_string1 = strings[0][:-1] + '-' + str1[next_cell[0] - 1]
+                new_string2 = strings[1] + str2[next_cell[1] - 1]
+                trace_back((new_string1, new_string2), x, y - 1)
+            elif direction == 'v':
+                new_string1 = strings[0] + str1[next_cell[0] - 1]
+                new_string2 = strings[1][:-1] + '-' + str2[next_cell[1] - 1]
+
+                trace_back((new_string1, new_string2), x - 1, y)
+
+    return
+
+
+def semi_global_alignment():
+    for loc in total_locations:
+        x, y = loc[0], loc[1]
+        string1 = '' + str1[x - 1]
+        string2 = '' + str2[y - 1]
+
+        # TODO add gaps to start of strings if needed
+        for i in range(x, len(str1)):
+            string1 = str1[i] + string1
+            string2 = '-' + string2
+        for i in range(y, len(str2)):
+            string1 = '-' + string1
+            string2 = str2[i] + string2
+
+        trace_back((string1, string2), x, y)
+
+
+# Find cells with total score
+def find_total_score_locations():
+    global total_locations
+
+    for i in range(len(str1) + 1):
+        for j in range(len(str2) + 1):
+            if score_matrix[i][j] >= total_score:
+                if i == len(str1) or j == len(str2):
+                    total_locations.append((i, j))
+
+
+def calculate_score(arr, i, j):
+    temp_direction = ''
+    temp_score = 0
+
+    if (not i == 0) and (not j == 0):
+        diagonal = arr[i - 1][j - 1] + PAM250[str1[i - 1]][str2[j - 1]]
+        horizontal = arr[i][j - 1] + GAP_PENALTY
+        vertical = arr[i - 1][j] + GAP_PENALTY
+
+        temp_score = max(diagonal, horizontal, vertical)
+
+        # Find total score in last row and column
+        global total_score
+        if temp_score >= total_score:
+            if i == len(str1) or j == len(str2):
+                total_score = temp_score
+
+        if temp_score == diagonal:
+            temp_direction += 'd'
+        if temp_score == horizontal:
+            temp_direction += 'h'
+        if temp_score == vertical:
+            temp_direction += 'v'
+
+    return temp_score, temp_direction
+
+
+def init_and_fill_matrix():
+    for i in range(len(str1) + 1):
+        temp_score_arr = []
+        temp_direction_arr = []
+        score_matrix.append(temp_score_arr)
+        direction_matrix.append(temp_direction_arr)
+
+        for j in range(len(str2) + 1):
+            score, direction = calculate_score(score_matrix, i, j)
+
+            score_matrix[score_matrix.index(temp_score_arr)].append(score)
+            direction_matrix[direction_matrix.index(temp_direction_arr)].append(direction)
+
+    # for i in range(len(str1) + 1):
+    #     if i == 0:
+    #         print('λ', end="")
+    #     else:
+    #         print(str1[i - 1], end="")
+    #
+    #     print(score_matrix[i])
+    # for i in range(len(str1) + 1):
+    #     print(direction_matrix[i])
+
+
+if __name__ == '__main__':
+    str1 = input()
+    str2 = input()
+
+    # str1 = 'HEAGAWGHE'
+    # str2 = 'PAWHEA'
+    # str1 = 'AAAAA'
+    # str2 = 'AA'
+
+    init_and_fill_matrix()
+    find_total_score_locations()
+    semi_global_alignment()
+
+    # print(seq)
+    print_output(total_score, seq)
